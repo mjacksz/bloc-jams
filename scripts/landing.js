15b4d051fb8057f700183dcb2f253cb09cfaa7c3
Martin Jackson <l3tmein.6@gmail.com>
2017-04-20 14:19:03 -0700

    Checkpoint-9 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,8 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
- var points = document.getElementsByClassName('point');
- 
- var revealPoint = function(index)  { 
+var animatePoints = function(points) {
+
+ var revealPoint = function(index)  {     
     points[index].style.opacity = 1;
     points[index].style.transform = "scaleX(1) translateY(0)";
     points[index].style.msTransform = "scaleX(1) translateY(0)";
@@ -13,3 +13,19 @@ var animatePoints = function() {
         revealPoint(i);            
     }
 };
+
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+        }
+    });
+ }
